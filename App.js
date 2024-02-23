059a79b2886b0d644dfa100b578b716404b5133b
navodita.tandon <navodita.tandon@myntra.com>
2024-02-23 12:07:43 +0530

    code changes

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = <h1 id="heading">My React using JSX</h1>;
+/**
+ * Header: 
+ *  - Logo
+ *  - Nav Items
+ * 
+ * Body:
+ *  - Search
+ *  - Restaurant Container
+ *  - Restaurant Card
+ *          - Image
+ *          - Name of the restaurant + star rating + cuisine + delivery time.
+ * 
+ * Footer:
+ *  - Copyright
+ *  - links
+ *  - Address
+ *  - Contact
+ */
+const Header = () => {
+    return(
+        <div className = "header">
+            <div className = "logo-container">
+                <img className = "logo" src = "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
-const title = <h1 className="heading">My react Functional Component Title</h1>
+const styleCard = {
+    backgroundColor : "#f0f0f0",
+};
 
-const number = 10000;
+const RestaurantCard = (props) => {
+    return (
+        <div className="res-card" style={styleCard}>
+            <img
+             className="res-logo"
+             alt = "res-logo"
+             src = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xqwpuhgnsaf18te7zvtv"/>
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>4.4 stars</h4>
+            <h4>38 minutes</h4>
+        </div>
+    )
+}
 
-const FunctionalComponent = () => (
-<div>
-    {title}
-    {number}
-    <h1 className="heading">My react Functional Component</h1>
-</div>
-)
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard resName = "Meghna Foods" cuisine = "Biryani, North Indian, Asian"/>
+                <RestaurantCard resName = "KFC" cuisine = "Burger, Fast Food"/>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return(
+        <div className = "app">
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<FunctionalComponent/>);
-
-
+root.render(<AppLayout/>);
